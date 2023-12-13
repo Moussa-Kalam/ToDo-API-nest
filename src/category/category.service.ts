fe6529522ff7e3b6d6f5acac12bf3506c99ef990
Moussa-Kalam <m.amzat@alustudent.com>
2023-12-13 22:26:02 +0200

    feat: handle duplicate categories and throw user-friendly error message

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { CategoryRepository } from './category.repository';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { db } from 'src/db/database';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoryService {
@@ -13,11 +14,23 @@ export class CategoryService {
   }
 
   async create(createCategoryDto: CreateCategoryDto) {
+    const categories = await this.findAll();
+
     const id = uuidv4();
     const newCategory = {
       id,
       ...createCategoryDto,
     };
+
+    const categoryName = createCategoryDto.name;
+    const existingCategory: Category = categories.find(
+      (category: Category) => category.name === categoryName,
+    );
+
+    if (existingCategory && categoryName === existingCategory.name)
+      throw new NotFoundException(
+        `${categoryName} category already exists in the database`,
+      );
 
     return this.categoryRepository.create(newCategory);
   }
